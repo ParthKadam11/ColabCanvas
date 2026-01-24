3f1d7173257bd4c7bb28b0a497bdb3b1ff51f868
ParthKadam11 <parthkadamalt@gmail.com>
2026-01-24 20:12:21 +0530

    feat: add common package with validation schemas and update dependencies in backend apps

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,31 +1,55 @@
+import "@repo/types"
 import express from "express";
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
+import { CreateRoomSchema, SigninSchema } from "@repo/common/types.ts";
 
 const JWT_SECRET =process.env.JWT_SECRET
 
 const app=express()
-
 app.use(express.json())
+
 app.post("/",(req,res)=>{
     res.json("Hello World")
 })
 
 app.post("/signup",(req,res)=>{
-    const {username,password} = req.body
-    
+    const result = CreateRoomSchema.safeParse(req.body)
+    if(!result.success){
+        res.json({
+            message:"Incorrect Input"
+        })
+        return
+    }
+    //dbcall
+    res.json({
+        userId:"123"
+    })
 })
 
 app.post("/signin",(req,res)=>{
-    const {username,password} =req.body
-    const token=jwt.sign(username,JWT_SECRET as string)
-
+    const result = SigninSchema.safeParse(req.body);
+    if (!result.success) {
+    res.json({
+        message: "Incorrect Credentials",
+    });
+    return;
+    }    
+    const {username} = result.data 
+    const token=jwt.sign({username:username},JWT_SECRET as string)
     res.json({
         token
     })
 })
 
 app.post("/room", middleware ,(req,res)=>{
+    const result = CreateRoomSchema.safeParse(req.body)
+    if(!result.success){
+        res.json({
+            message:"Incorrect Input"
+        })
+        return
+    }
     res.json({
         RoomId:"123"
     })
