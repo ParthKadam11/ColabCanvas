e4c90cb9bee8290752c147a0b04f213c7922ac95
ParthKadam11 <parthkadamalt@gmail.com>
2026-02-17 15:38:58 +0530

    fix: correct import paths for Button, Card, Code, and Input components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,5 @@
-export { Button } from "./Button";
-export { Card } from "./Card";
-export { Code } from "./Code";
+export { Button } from "./button";
+export { Card } from "./card";
+export { Code } from "./code";
 export { IconButton } from "./IconButton";
-export { ForwardedInput as Input } from "./Input";
+export { ForwardedInput as Input } from "./input";
